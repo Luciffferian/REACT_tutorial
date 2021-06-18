66bb5e50aad9463e0027541b7081ae9e78b025fc
Alika <alika.sveca@gmail.com>
2021-06-18 09:10:27 +0300

    last commit

--- homework/script.js
+++ homework/script.js
@@ -9,9 +9,9 @@ personalMovieDB = {
     privat: false
 },
 lastMovie1 = prompt('Один из последних просмотренных фильмов?', ''),
-lastMovieRank1 = +prompt('Нф сколько оцените его?', '1-10'),
+lastMovieRank1 = +prompt('На сколько оцените его?', '1-10'),
 lastMovie2 = prompt('Один из последних просмотренных фильмов?', ''),
-lastMovieRank2 = +prompt('Нф сколько оцените его?', '1-10');
+lastMovieRank2 = +prompt('На сколько оцените его?', '1-10');
 
 personalMovieDB.movies[lastMovie1] = lastMovieRank1;
 personalMovieDB.movies[lastMovie2] = lastMovieRank2;
